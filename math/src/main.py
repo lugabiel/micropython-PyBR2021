48a85442f9b1ee8d737471fea438573876daf57f
gabriel aragão <lugabiel@hotmail.com>
2021-10-17 22:54:18 -0300

    math example

---
+++ math/src/main.py
@@ -0,0 +1,11 @@
+# main.py
+# a subset of the Python Math library
+
+import math
+import cmath
+
+print(math.sqrt(5))
+print(math.log10(100))
+print(math.sin(12345) ** 2 + math.cos(12345) ** 2)
+print(math.cosh(1) ** 2 - math.sinh(1) ** 2)
+print(cmath.polar(1 + 1j))
